f67de9875ddf440ed59ef5f319ebf15efadb73b3
Ryker <zhangy42@rose-hulman.edu>
2020-11-07 22:25:41 +0800

    Moving Smile Drawing Step

--- 00-MovingSmile/MovingSmile.py
+++ 00-MovingSmile/MovingSmile.py
@@ -21,7 +21,9 @@ def main():
         pygame.draw.circle(screen, (255, 255, 0), (320, 240), 210)  # yellow circle
         pygame.draw.circle(screen, (0, 0, 0), (320, 240), 210, 4)  # black outline
 
-        pygame.draw.circle(screen, (225, 225, 225), (240, 160), 25)  # white eye
+
+
+        pygame.draw.circle(screen, (225, 225, 255), (240, 160), 25)  # white eye
         pygame.draw.circle(screen, (0, 0, 0), (240, 160), 25, 3)  # black outline
         pygame.draw.circle(screen, (0, 0, 0), (242, 162), 7)  # black pupil
 
@@ -32,10 +34,11 @@ def main():
         # TODO 1: Draw a nose
         # Suggestion: color (80,0,0) location (320,245), radius 10
         # API --> pygame.draw.circle(screen, (r,g,b), (x, y), radius, thickness)
-
+        pygame.draw.circle(screen,(80,0,0),(320,245),10)
         # TODO 2: Draw a mouth
         # Suggestion: color (0,0,0), x 230, y 320, width 180, height 30
         # API --> pygame.draw.rect(screen, (r,g,b), (x, y, width, height), thickness)
+        pygame.draw.rect(screen,(0,0,0),(230,320,180,30))
 
         pygame.display.update()
 
